cf4ea97e42ffb6a680bd24490bc5b4f9fab8e1dd
resbrandao <resbrandao@gmail.com>
2023-02-18 12:10:57 -0300

    Aplicação Spring com RabbitMQ
    Aplicação Spring com RabbitMQ

---
+++ src/main/java/br/com/fiap/spring/service/AgendamentoService.java
@@ -0,0 +1,11 @@
+package br.com.fiap.spring.service;
+
+import br.com.fiap.spring.dto.AgendamentoDTO;
+
+import java.util.List;
+
+public interface AgendamentoService {
+
+    List<AgendamentoDTO> getAgendamento();
+
+}
